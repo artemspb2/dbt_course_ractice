eaecec1ebdd561b3174ffcbfb4b11d7dd9b3172f
artemspb2 <artemspb2@gmail.com>
2025-06-27 10:21:20 +0500

    - created fact tables, created new stg tables

---
+++ models/staging/flights/stg_flights__ticket_flights.sql
@@ -0,0 +1,18 @@
+{{
+    
+      config(
+        materialized = 'table'
+        )
+    
+}}
+      select
+        ticket_no,
+        flight_id,
+        fare_conditions,
+        amount
+
+      from source
+  )
+from {{ source('demo_src', 'ticket_flights') }}
+  
+    
